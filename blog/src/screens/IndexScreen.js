f358d67ae4601e3d0091630a91cb5eb4f74c282d
Rachel Wong <rwrachelwong@gmail.com>
2020-08-26 11:19:16 -0700

    using useContext in blog project

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-
+import React, { useContext } from 'react'
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
+import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
+  const {data, addBlogPost } = useContext(BlogContext)
+
   return <View>
     <Text>Index Screen</Text>
-  </View>
+    <Button title="Add Post" onPress={addBlogPost}/>
+    <FlatList 
+      data={data}
+      keyExtractor={blogPosts => blogPosts.title}
+      renderItem={({ item }) => {
+      return <Text>{item.title}</Text>
+      }}
+    />
+    </View>
 }
 
 const styles = StyleSheet.create({})
