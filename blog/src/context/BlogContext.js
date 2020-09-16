b4d3c6575873df69f57ae353f8ab498294867a13
Rachel Wong <rwrachelwong@gmail.com>
2020-09-15 18:39:08 -0700

    add delete blog btn and show blog screen

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -6,7 +6,12 @@ import createDataContext from './createDataContext'
 const blogReducer = (state, action) => {
   switch (action.type) {
     case 'add':
-      return [...state, {title: `Blog Post #${state.length + 1}`}]
+      return [...state, {
+        id: Math.floor(Math.random()* 99999), 
+        title: `Blog Post #${state.length + 1}`
+      }]
+    case 'delete_blogpost':
+      return state.filter(blogPost => blogPost.id !== action.payload )
     default:
       return state
   }
@@ -18,8 +23,14 @@ const addBlogPost = (dispatch) => {
   }
 }
 
+const deleteBlogPost = dispatch => {
+  return (id) => {
+    dispatch({ type: 'delete_blogpost', payload: id }) // naming conventions for obj
+  }
+}
+
 export const { Context, Provider } = createDataContext(
   blogReducer, 
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 )
